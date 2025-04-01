043e0d66ed97b821f65d80a096d9f51640d88a57
unknown <oit@DESKTOP-LHG1T76.ad.ametrin.com.ua>
2025-04-01 21:17:45 +0300

    Created Api structure, for printers, where you can use SMNPv1 to get values from printers

---
+++ MonitoringPrinters/Model/ConsumablesInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace MonitoringPrinters.Model
+{
+    public class ConsumablesInfo
+    {
+        public int? TonerLevel { get; set; }
+        public int? MaxTonerLevel { get; set; }
+        public Dictionary<string, int> VendorSpecificConsumables { get; set; } = new Dictionary<string, int>();
+    }
+}
